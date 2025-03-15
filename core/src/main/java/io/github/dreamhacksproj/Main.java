558cc9821e475fda5fc0bf0a68525f397e4c2a27
Alezis69 <a.bezbocov@gmail.com>
2025-03-15 12:07:25 -0700

    maze

--- core/src/main/java/io/github/dreamhacksproj/Main.java
+++ core/src/main/java/io/github/dreamhacksproj/Main.java
@@ -7,9 +7,15 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
+import io.github.dreamhacksproj.Maze;
+import io.github.dreamhacksproj.Node;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
+    private static final int TILE_WIDTH = 24;
+    private static final int TILE_HEIGHT = 24;
+    private static final int MAP_WIDTH = 32;
+    private static final int MAP_HEIGHT = 32;
     private SpriteBatch batch;
 
     @Override
@@ -18,16 +24,15 @@ public class Main extends ApplicationAdapter {
         AnimationsLoader loader = new AnimationsLoader();
 
     }
-
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
         batch.begin();
         // batch.draw
 
         batch.end();
     }
-
     @Override
     public void dispose() {
         batch.dispose();
