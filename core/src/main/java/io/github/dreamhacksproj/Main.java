2794b6da7e0fa56d8eee7663fe3e7b8cbda0f623
Alezis69 <a.bezbocov@gmail.com>
2025-03-15 14:14:12 -0700

    maze

--- core/src/main/java/io/github/dreamhacksproj/Main.java
+++ core/src/main/java/io/github/dreamhacksproj/Main.java
@@ -3,6 +3,7 @@ package io.github.dreamhacksproj;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -12,10 +13,10 @@ import io.github.dreamhacksproj.Node;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    private static final int TILE_WIDTH = 24;
-    private static final int TILE_HEIGHT = 24;
-    private static final int MAP_WIDTH = 32;
-    private static final int MAP_HEIGHT = 32;
+    private static final int TILE_WIDTH = 16;
+    private static final int TILE_HEIGHT = 16;
+    private static final int MAP_WIDTH = 40;
+    private static final int MAP_HEIGHT = 40;
     private SpriteBatch batch;
 
     @Override
@@ -26,8 +27,9 @@ public class Main extends ApplicationAdapter {
     }
     @Override
     public void render() {
+        camera.update();
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-
+        batch.setProjectionMatrix(camera.combined);
         batch.begin();
         // batch.draw
 
